b51bc29a916f34ed69e75abe254e7128bf113c1f
RasanduniLakmali <rasandunilakmali26@gmail.com>
2025-08-13 14:29:48 +0530

    updated App,main pages

--- Book_Library_Frontend/src/main.tsx
+++ Book_Library_Frontend/src/main.tsx
@@ -5,12 +5,15 @@ import "./index.css"
 import {AuthProvider} from "./context/AuthProvider.tsx";
 import {RouterProvider} from "react-router-dom";
 import router from "./router.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <AuthProvider>
           <RouterProvider router={router}/>
+          <ToastContainer position="top-right" autoClose={3000} />
       </AuthProvider>
 
-  </StrictMode>,
+  </StrictMode>
 )
